6f738ad3af648abf0d76078e5bb0832b65e1b135
unknown <onkar.kulkarni@coditas.com>
2022-03-02 16:34:34 +0530

    New Changes

--- 09-Feb&10-Feb/CS_Imperative_LINQ/EmployeeSalarySlips.cs
+++ 09-Feb&10-Feb/CS_Imperative_LINQ/EmployeeSalarySlips.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 using Humanizer;
 
 namespace CS_Imperative_LINQ
@@ -29,37 +30,65 @@ namespace CS_Imperative_LINQ
             // The File will be Open if it exists: FileMode.OpenOrCreate
 
             // The File is Opened for the Write Operations: FileAccess.Write  
+            //FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+
+            /* // 2. Create an Instance of StreamWriter
+             StreamWriter writer = new StreamWriter(fs);
+
+             // 3. Write Data to Stream
+             writer.WriteLine("" +
+        "------------------------Salary Slip for Month of Feb---------------------------" +
+     $"\n| EmpNo: {EmpNo}          EmpName: {EmpName}         DeptName: {DeptName}     |" +
+     $"\n| Designation:  {Designation}                                                 |" +
+     "\n|------------------------------------------------------------------------------|" +
+     "\n|Income (Rs.)					| Deduction (Rs.)				              |" +
+     "\n|------------------------------- |---------------------------------------------|" +
+     $"\n|Basic Salary:	{Salary}		|								              |" +
+     $"\n|HRA:			{HRA}			|								              |" +
+     $"\n|TA:            {TS}			|								              |" +
+     $"\n|DA:            {DA}			|								              |" +
+     "\n|--------------------------------|---------------------------------------------|" +
+     $"\n| Gross:        {Gross}         |								              |" +
+     "\n|--------------------------------|---------------------------------------------|" +
+     $"\n|                                Tax:			{Tax}			              |" +
+     "\n|--------------------------------|---------------------------------------------|" +
+     $"\n|NetSalary:		{NetSalary}		|								              |" +
+     "\n|------------------------------------------------------------------------------|" +
+     $"\n|NetSalary In Words:{NetSalary.ToWords()}				              |" +
+     "\n|------------------------------------------------------------------------------|");
+
+             // 4. Close Stream to Make sure that the Stream of data is commited to file
+             writer.Close();
+
+             // 5. Close Stream to Commit the File
+             fs.Close();*/
+
+
             FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter formatter = new BinaryFormatter();
+            //FileStream stream= new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            byte[] content = new UTF8Encoding(true).GetBytes(
+                          "------------------------Salary Slip for Month of Feb---------------------------" +
+     $"\n| EmpNo: {EmpNo}          EmpName: {EmpName}         DeptName: {DeptName}     |" +
+     $"\n| Designation:  {Designation}                                                 |" +
+     "\n|------------------------------------------------------------------------------|" +
+     "\n|Income (Rs.)					| Deduction (Rs.)				              |" +
+     "\n|------------------------------- |---------------------------------------------|" +
+     $"\n|Basic Salary:	{Salary}		|								              |" +
+     $"\n|HRA:			{HRA}			|								              |" +
+     $"\n|TA:            {TS}			|								              |" +
+     $"\n|DA:            {DA}			|								              |" +
+     "\n|--------------------------------|---------------------------------------------|" +
+     $"\n| Gross:        {Gross}         |								              |" +
+     "\n|--------------------------------|---------------------------------------------|" +
+     $"\n|                                Tax:			{Tax}			              |" +
+     "\n|--------------------------------|---------------------------------------------|" +
+     $"\n|NetSalary:		{NetSalary}		|								              |" +
+     "\n|------------------------------------------------------------------------------|" +
+     $"\n|NetSalary In Words:{NetSalary.ToWords()}				                                         |" +
+     "\n|------------------------------------------------------------------------------|");
 
-            // 2. Create an Instance of StreamWriter
-            StreamWriter writer = new StreamWriter(fs);
-
-            // 3. Write Data to Stream
-            writer.WriteLine("" +
-       "------------------------Salary Slip for Month of Feb---------------------------" +
-    $"\n| EmpNo: {EmpNo}          EmpName: {EmpName}         DeptName: {DeptName}     |" +
-    $"\n| Designation:  {Designation}                                                 |" +
-    "\n|------------------------------------------------------------------------------|" +
-    "\n|Income (Rs.)					| Deduction (Rs.)				              |" +
-    "\n|------------------------------- |---------------------------------------------|" +
-    $"\n|Basic Salary:	{Salary}		|								              |" +
-    $"\n|HRA:			{HRA}			|								              |" +
-    $"\n|TA:            {TS}			|								              |" +
-    $"\n|DA:            {DA}			|								              |" +
-    "\n|--------------------------------|---------------------------------------------|" +
-    $"\n| Gross:        {Gross}         |								              |" +
-    "\n|--------------------------------|---------------------------------------------|" +
-    $"\n|                                Tax:			{Tax}			              |" +
-    "\n|--------------------------------|---------------------------------------------|" +
-    $"\n|NetSalary:		{NetSalary}		|								              |" +
-    "\n|------------------------------------------------------------------------------|" +
-    $"\n|NetSalary In Words:{NetSalary.ToWords()}				              |" +
-    "\n|------------------------------------------------------------------------------|");
-
-            // 4. Close Stream to Make sure that the Stream of data is commited to file
-            writer.Close();
-
-            // 5. Close Stream to Commit the File
+            formatter.Serialize(fs, content);
             fs.Close();
         }
     }
